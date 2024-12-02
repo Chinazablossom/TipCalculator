60a893cefa651b0ce19b6e8dbc26f475693a6971
chinazablossom <chinazablossom7@gmail.com>
2024-12-03 00:27:55 +0100

    Stage 2

--- Tip Calculator/task/src/main/java/org/hyperskill/calculator/tip/MainActivity.kt
+++ Tip Calculator/task/src/main/java/org/hyperskill/calculator/tip/MainActivity.kt
@@ -1,8 +1,12 @@
 package org.hyperskill.calculator.tip
 
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import org.hyperskill.calculator.tip.databinding.ActivityMainBinding
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -12,6 +16,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.apply {
 
+            editText.doOnTextChanged { text, start, before, count ->
+                when {
+                    text.isNullOrBlank() || text.toString().toBigDecimal() <= BigDecimal.ZERO -> {
+                        billValueTv.text = ""
+                        tipPercentTv.text = ""
+                    }
+
+                    else -> {
+                        billValueTv.text =
+                            getString(R.string.billValue, text.toString().toBigDecimal())
+                        tipPercentTv.text = String.format("Tip: %s%%", seekBar.progress.toString())
+                    }
+                }
+            }
+
+            seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+                override fun onProgressChanged(s: SeekBar?, progressValue: Int, p2: Boolean) {
+                    tipPercentTv.text =
+                        if (binding.editText.text.isNullOrBlank()) "" else getString(
+                            R.string.percent,
+                            s?.progress
+                        )
+                }
+
+                override fun onStartTrackingTouch(s: SeekBar?) {}
+
+                override fun onStopTrackingTouch(s: SeekBar?) {}
+
+            })
+
         }
     }
+
 }
