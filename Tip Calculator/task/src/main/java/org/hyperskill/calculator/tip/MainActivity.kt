424522884561976ad7dc781afa16de50d7fc7514
chinazablossom <chinazablossom7@gmail.com>
2024-12-03 03:35:16 +0100

    Stage 3

--- Tip Calculator/task/src/main/java/org/hyperskill/calculator/tip/MainActivity.kt
+++ Tip Calculator/task/src/main/java/org/hyperskill/calculator/tip/MainActivity.kt
@@ -16,28 +16,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.apply {
 
-            editText.doOnTextChanged { text, start, before, count ->
+            editText.doOnTextChanged { text, _, _, _ ->
                 when {
                     text.isNullOrBlank() || text.toString().toBigDecimal() <= BigDecimal.ZERO -> {
                         billValueTv.text = ""
                         tipPercentTv.text = ""
+                        tipAmountTv.text = ""
                     }
 
                     else -> {
-                        billValueTv.text =
-                            getString(R.string.billValue, text.toString().toBigDecimal())
+                        billValueTv.text = getString(R.string.billValue, text.toString().toBigDecimal())
                         tipPercentTv.text = String.format("Tip: %s%%", seekBar.progress.toString())
+                        tipAmountTv.text = getString(R.string.amount, BigDecimal(text.toString().toDouble() * seekBar.progress.toDouble() / 100 )  )
                     }
                 }
             }
 
             seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
                 override fun onProgressChanged(s: SeekBar?, progressValue: Int, p2: Boolean) {
-                    tipPercentTv.text =
-                        if (binding.editText.text.isNullOrBlank()) "" else getString(
-                            R.string.percent,
-                            s?.progress
-                        )
+
+                    tipPercentTv.text = if (binding.editText.text.isNullOrBlank()) "" else getString(R.string.percent, s?.progress)
+                    tipAmountTv.text = if (binding.editText.text.isNullOrBlank()) "" else getString(R.string.amount, BigDecimal(editText.text.toString().toDouble() * seekBar.progress.toDouble() / 100 )  )
                 }
 
                 override fun onStartTrackingTouch(s: SeekBar?) {}
